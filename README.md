b966e748ea11781aed49d7d4c0281ac61086353f
Aries McRae <aries@ariesmcrae.com>
2014-06-29 23:52:53 +1000

    Wiped default main.html content.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ RedditClone
 ===========
 
 A Reddit clone written in AngularJS
+
+##Pre-requisites
+* NodeJs
+* Npm
+* Grunt
+* Bower
+* Karma
+
+##How to run
+* Clone this repo.
+* From the command line, execute 'npm install'
+* Execute 'grunt serve'
+* launch localhost:9000
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,25 @@
-'use strict';
+(function() {
 
-/**
- * @ngdoc function
- * @name redditCloneApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the redditCloneApp
- */
-angular.module('redditCloneApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name redditCloneApp.controller:MainCtrl
+	 * @description
+	 * # MainCtrl
+	 * Controller of the redditCloneApp
+	 */
+
+	var MainCtrl = function ($scope) {
+		$scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma' ];
+	};
+
+	//workaround so that we're still safe after JS minification for production.
+	MainCtrl.$inject = ['$scope'];
+
+	//register controller with angular
+	angular.module('redditCloneApp').controller('MainCtrl', MainCtrl);
+
+
+}());
+
