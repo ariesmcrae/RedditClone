ac47f0b94dd09818f4eaffb7388f16a0ef68944b
Aries McRae <aries@ariesmcrae.com>
2014-06-29 22:18:31 +1000

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name redditCloneApp
+ * @description
+ * # redditCloneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('redditCloneApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
